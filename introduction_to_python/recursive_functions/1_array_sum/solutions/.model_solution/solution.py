1235bad37c3955ec07dd14d12f16838e58026823
Lester Kim <lester.c.kim@gmail.com>
2020-07-17 22:58:18 -0400

    style(1_array_sum): Add typing and optimize list recursion

--- introduction_to_python/recursive_functions/1_array_sum/solutions/.model_solution/solution.py
+++ introduction_to_python/recursive_functions/1_array_sum/solutions/.model_solution/solution.py
@@ -1,2 +1,6 @@
-def sum_array(num_list):
-    return 0 if num_list == [] else num_list[0] + sum_array(num_list[1:])
+from typing import List, Union
+
+
+def sum_array(numbers: List[Union[float, int]]) -> int:
+    return 0 if numbers == [] else numbers.pop() + sum_array(numbers)
+

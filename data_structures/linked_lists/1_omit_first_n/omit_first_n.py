f87bf618c75bac4db5d76fcb583e866ebf63390b
Lester Kim <lester.c.kim@gmail.com>
2020-07-19 06:27:42 -0400

    style(omit_first_n.py): Use recursion

--- data_structures/linked_lists/1_omit_first_n/omit_first_n.py
+++ data_structures/linked_lists/1_omit_first_n/omit_first_n.py
@@ -13,21 +13,20 @@ class Node:
         self.rest = rest
         self._length = 0
 
-    def _calculate_length(self) -> int:
-        self._length = 0
-        node = self
-
-        while node is not None:
-            self._length += 1
-            node = node.rest
-
+    def _calculate_length(self, node) -> int:
+        self._length = 0 if node is None else 1 + self._calculate_length(node.rest)
         return self._length
 
     def __len__(self) -> int:
-        return self._length if self._length else self._calculate_length()
+        return self._length if self._length else self._calculate_length(self)
 
 
 # Write a function called omit_first_n that consumes a LinkedList linked_list and an integer n. The function returns a LinkedList with the first n elements omitted or None if n > len(ll)
 # omit_first_n(Node(1, Node(2, Node(3, None))), 2) -> Node(3, None)
 def omit_first_n(linked_list: Optional[Node], n: int) -> Optional[Node]:
     return linked_list if n == 0 or linked_list is None else omit_first_n(linked_list.rest, n - 1)
+
+
+linked_list = Node(1, Node(2))
+assert len(linked_list) == 2
+assert omit_first_n(linked_list, 2) == None

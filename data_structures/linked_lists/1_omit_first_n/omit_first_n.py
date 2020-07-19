4c9e3e8535e4584a564efc497f23b331f842d078
Lester Kim <lester.c.kim@gmail.com>
2020-07-19 06:47:18 -0400

    refactor(omit_first_n.py): Refactor __len__

--- data_structures/linked_lists/1_omit_first_n/omit_first_n.py
+++ data_structures/linked_lists/1_omit_first_n/omit_first_n.py
@@ -11,22 +11,12 @@ class Node:
     def __init__(self, first: int, rest = None) -> None:
         self.first = first
         self.rest = rest
-        self._length = 0
-
-    def _calculate_length(self, node) -> int:
-        self._length = 0 if node is None else 1 + self._calculate_length(node.rest)
-        return self._length
 
     def __len__(self) -> int:
-        return self._length if self._length else self._calculate_length(self)
+        return 1 if self.rest is None else 1 + len(self.rest)
 
 
 # Write a function called omit_first_n that consumes a LinkedList linked_list and an integer n. The function returns a LinkedList with the first n elements omitted or None if n > len(ll)
 # omit_first_n(Node(1, Node(2, Node(3, None))), 2) -> Node(3, None)
 def omit_first_n(linked_list: Optional[Node], n: int) -> Optional[Node]:
     return linked_list if n == 0 or linked_list is None else omit_first_n(linked_list.rest, n - 1)
-
-
-linked_list = Node(1, Node(2))
-assert len(linked_list) == 2
-assert omit_first_n(linked_list, 2) == None

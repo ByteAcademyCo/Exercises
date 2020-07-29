3b8cef1d11de80ec9f8bca6529eceb87b63f35a2
PC-coding <partapchauhan8@gmail.com>
2020-07-29 14:54:27 -0400

    Exercises completed

--- introduction_and_environment/hello_world/1_concantenation/Solution/solution.py
+++ introduction_and_environment/hello_world/1_concantenation/Solution/solution.py
@@ -1,1 +1,5 @@
 # Code your solution here
+string_1 = "hello"
+string_2 = "world!"
+result = string_1 + string_2
+print(result)
--- introduction_and_environment/hello_world/3_user_input/solution/solution.py
+++ introduction_and_environment/hello_world/3_user_input/solution/solution.py
@@ -1,1 +1,7 @@
 # Code your solution here
+first_num = input("State one number:")
+second_num = input("State another number:")
+third_num = input("State a third number:")
+
+result = (first_num * second_num * third_num) / 3
+print(result) 

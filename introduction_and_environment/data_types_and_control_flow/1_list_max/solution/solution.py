44800a1ba0ffe8da75e0b28c2352282cc011a918
PC-coding <partapchauhan8@gmail.com>
2020-07-31 16:53:18 -0400

    Completed

--- introduction_and_environment/data_types_and_control_flow/1_list_max/solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_list_max/solution/solution.py
@@ -1,11 +1,19 @@
-L = [152, 463, 1112, 1337, -10]
-# def LIST_MAX(L):
-#     if L == []:
-#         LIST_MAX1 = None
-#     else:
-#         LIST_MAX1 = sorted(L)[-1]
-#     return LIST_MAX1
+# L = [152, 463, 1112, 1337, -10]
+# LIST_MAX = sorted(L)[-1]
 
 # print(LIST_MAX(L))
-LIST_MAX = sorted(L)[-1]
-# print(LIST_MAX)
+
+L = [152, 463, 1112, 1337, -10]
+def LIST_MAX1(L):
+    current_max = L[0]
+    if L[1] > current_max:
+        current_max = L[1]
+    if L[2] > current_max:
+        current_max = L[2]
+    if L[3] > current_max:
+        current_max = L[3]
+    if L[4] > current_max:
+        current_max = L[4]
+    return current_max
+LIST_MAX = LIST_MAX1(L)
+
--- introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
@@ -1,1 +1,10 @@
 # Code your solution here
+figure = input('Enter any geometric shape value:')
+my_dict = {'3':'Triangle', '4':'Quadrilateral', '5':'Pentagon', '6':'Hexagon', '7':'Heptagon', '8':'Octagon', '9':'Nonagon'}
+
+def data(figure):
+    if figure in my_dict:
+        return my_dict[figure]
+    else:
+        return 'None'
+print(data(figure))

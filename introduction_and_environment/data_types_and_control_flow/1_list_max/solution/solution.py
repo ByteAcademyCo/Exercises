68fe6b106aa65fffc3fb9a2e8108741bd7113a45
PC-coding <partapchauhan8@gmail.com>
2020-07-31 11:10:49 -0400

    Pushing exercises

---
+++ introduction_and_environment/data_types_and_control_flow/1_list_max/solution/solution.py
@@ -0,0 +1,11 @@
+L = [152, 463, 1112, 1337, -10]
+# def LIST_MAX(L):
+#     if L == []:
+#         LIST_MAX1 = None
+#     else:
+#         LIST_MAX1 = sorted(L)[-1]
+#     return LIST_MAX1
+
+# print(LIST_MAX(L))
+LIST_MAX = sorted(L)[-1]
+# print(LIST_MAX)
--- introduction_and_environment/data_types_and_control_flow/3_set_difference/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_set_difference/Solution/solution.py
@@ -1,1 +1,5 @@
 # Code your solution here
+set_1 = set()
+set_2 = set()
+result = set_1 - set_2
+print(result)

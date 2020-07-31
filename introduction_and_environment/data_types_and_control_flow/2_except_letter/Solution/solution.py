d536594f57b40a0ff66468064899bc12f9e42907
PC-coding <partapchauhan8@gmail.com>
2020-07-31 11:10:49 -0400

    Pushing exercises

--- introduction_and_environment/data_types_and_control_flow/2_except_letter/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_except_letter/Solution/solution.py
@@ -1,1 +1,11 @@
 # Code your solution here
+string = 'BYTE ACADEMY'
+vowels = ['A', 'E', 'I', 'O', 'U']
+def data(x):
+    ans = False
+    for characters in string:
+        if characters != vowels:
+            ans = True
+    return ans
+print(data)
+        

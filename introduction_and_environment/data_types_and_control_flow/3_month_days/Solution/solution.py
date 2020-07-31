f07cce675b51ee39b0fd16aeb26541dfbf8612ff
PC-coding <partapchauhan8@gmail.com>
2020-07-31 16:53:18 -0400

    Completed

--- introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
@@ -1,1 +1,10 @@
 # Code your solution here
+month = input('Enter any month:')
+month_dict = {'january':'31', 'february':'28, 29', 'march':'31', 'april':'30', 'may':'31', 'june':'30', 'july':'31', 'august':'31','september':'30', 'october':'31', 'november':'30', 'december':'31'}
+def data(month):
+    if month in month_dict.keys():
+        return month_dict[month]
+    else:
+        return None
+        
+print(data(month))

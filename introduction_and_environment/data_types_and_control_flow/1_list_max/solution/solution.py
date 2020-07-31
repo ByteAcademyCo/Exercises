a1dfc79c454c7a924ded0b29d6e8789ffcecf47c
PC-coding <partapchauhan8@gmail.com>
2020-07-31 16:53:18 -0400

    Completed

--- introduction_and_environment/data_types_and_control_flow/1_list_max/solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_list_max/solution/solution.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # L = [152, 463, 1112, 1337, -10]
 # LIST_MAX = sorted(L)[-1]
 
@@ -30,3 +31,24 @@ L = [152, 463, 1112, 1337, -10]
 LIST_MAX = sorted(L)[-1]
 # print(LIST_MAX)
 >>>>>>> d536594... Pushing exercises
+=======
+# L = [152, 463, 1112, 1337, -10]
+# LIST_MAX = sorted(L)[-1]
+
+# print(LIST_MAX(L))
+
+L = [152, 463, 1112, 1337, -10]
+def LIST_MAX1(L):
+    current_max = L[0]
+    if L[1] > current_max:
+        current_max = L[1]
+    if L[2] > current_max:
+        current_max = L[2]
+    if L[3] > current_max:
+        current_max = L[3]
+    if L[4] > current_max:
+        current_max = L[4]
+    return current_max
+LIST_MAX = LIST_MAX1(L)
+
+>>>>>>> f07cce6... Completed
--- introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
@@ -1,5 +1,6 @@
 # Code your solution here
 <<<<<<< HEAD
+<<<<<<< HEAD
 # length = len(input())
 # breadth = len(input())
 length = 6
@@ -9,5 +10,11 @@ length = 6
 breadth = 4
 
 >>>>>>> 39354d0... Completed Exercises
+=======
+# length = len(input())
+# breadth = len(input())
+length = 6
+breadth = 4
+>>>>>>> f07cce6... Completed
 data = length * breadth
 print(data)

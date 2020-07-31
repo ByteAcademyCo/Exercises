39354d0e1fa0fbb5999b2112a32ab6a78f665f82
PC-coding <partapchauhan8@gmail.com>
2020-07-30 20:11:09 -0400

    Completed Exercises

--- introduction_and_environment/data_types_and_control_flow/1_string_convert/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_string_convert/Solution/solution.py
@@ -1,1 +1,8 @@
 # Code your solution here
+string = str(input())
+def data(string):
+    if string.isupper():
+        string.lower
+    else:
+        string.upper
+print(data)
--- introduction_and_environment/data_types_and_control_flow/2_sum_list/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_sum_list/Solution/solution.py
@@ -1,1 +1,4 @@
 # Code your solution here
+list_a = []
+total = sum(list_a)
+print(total)

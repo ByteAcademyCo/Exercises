2f2edfb2dbd8830acd1a79c435563a9746b792ea
PC-coding <partapchauhan8@gmail.com>
2020-08-12 11:28:55 -0400

    Edited

--- data_structures/linked_lists/2_delete_item_value/solution/solution.py
+++ data_structures/linked_lists/2_delete_item_value/solution/solution.py
@@ -9,9 +9,27 @@ class linkedList:
         self.head = head
     def delete_item_by_value(self, x):
         current_node = self.head
-        previous_node = current_node
-        while(not current_node is None and not current_node.data is x):
+        previous_node = self.head
+        while current_node:
+            if current_node.data == x:
+                next_node = current_node.next
+                previous_node.next = next_node
+                return
             previous_node = current_node
             current_node = current_node.next
-        if not current_node is None:
-            previous_node.next = current_node.next
+        return 
+# 1 -> 2 -> 3 -> 4
+# 1 -> 2 -> 4
+# delete: 3
+
+# c_n = 1
+# p_n = 1
+
+# p_n = 1
+# c_n = 1+next = 2
+
+# p_n = 2
+# c_n = 2+next = 3
+
+# n_n = c_n + next = 4
+# p_n.next = 4

4366db8686ef1b9fb60b72ec135ffbb9e3de951b
PC-coding <partapchauhan8@gmail.com>
2020-08-12 11:39:52 -0400

    Edited

--- data_structures/linked_lists/1_insert_after/solution/solution.py
+++ data_structures/linked_lists/1_insert_after/solution/solution.py
@@ -10,17 +10,31 @@ class linkedList:
     def insert_after_item(self, x, data):
         current_node = self.head
         previous_node = self.head
-        if current_node is None:
-            new_node = Node(data)
-            self.head = new_node
-            return
-        while(not current_node is None and current_node.data is not x):
+        new_node = Node(data)
+        while current_node:
+            if current_node.data == x:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
             previous_node = current_node
             current_node = current_node.next
-        new_node = Node(data)
-        if current_node is None:
-            previous_node.next = new_node
-        else:
-            _next = current_node.next
-            current_node.next = new_node
-            new_node.next = _next
+        previous_node.next = new_node
+
+# 1 -> 2 -> 3 -> 5
+# insert: x=3 data=4
+
+# c_n = 1
+# p_n = 1
+# n_n = 4
+
+# p_n = c_n = 1
+# c_n = 1+next = 2
+
+# p_n = 1+next = 2
+# c_n = 2+next = 3
+
+# n_n.next = c_n.next = 5
+# 4        ->    5
+
+# c_n.next   =  n_n
+# 1 -> 2 -> 3 -> 4 -> 5

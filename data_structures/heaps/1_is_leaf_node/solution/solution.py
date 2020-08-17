c227cde29c58aa3f8d9acebff91b8032c5ed686c
PC-coding <partapchauhan8@gmail.com>
2020-08-17 11:17:27 -0400

    completed

--- data_structures/heaps/1_is_leaf_node/solution/solution.py
+++ data_structures/heaps/1_is_leaf_node/solution/solution.py
@@ -1,11 +1,5 @@
 # Write your solution here
 def is_leaf(heap, index): 
-        leaf_index = heap[-1]
-        if index == 0 and len(heap) > 0:
-                leaf_index[0]
-                return False
-        else:   
-                leaf_index
+        if index >= (len(heap)//2) and index < len(heap):
                 return True
-
-    
+        return False

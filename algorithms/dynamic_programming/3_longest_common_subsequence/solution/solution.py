f5e541dc04d38d7863a25cac847ede2126d5e1e4
PC-coding <partapchauhan8@gmail.com>
2020-08-21 12:16:42 -0400

    Completed

--- algorithms/dynamic_programming/3_longest_common_subsequence/solution/solution.py
+++ algorithms/dynamic_programming/3_longest_common_subsequence/solution/solution.py
@@ -1,2 +1,13 @@
 def longest_common_subseq(str1, str2):
-    return
+    length_1 = len(str1)
+    length_2 = len(str2)
+    Length = [[None]*(length_2 + 1) for i in range(length_1 + 1)]
+    for i in range(length_1 + 1):
+        for x in range(length_2 + 1):
+            if i == 0 or x == 0:
+                Length[i][x] = 0
+            elif str1[i-1] == str2[x-1]:
+                Length[i][x] = Length[i-1][x-1] + 1
+            else:
+                Length[i][x] = max(Length[i-1][x], Length[i][x-1])
+    return Length[length_1][length_2]
--- algorithms/dynamic_programming/3_palindromic_substring/solution/solution.py
+++ algorithms/dynamic_programming/3_palindromic_substring/solution/solution.py
@@ -1,2 +1,23 @@
 def longest_palindrome_substr(str):
-    return
+    n = len(str)
+    if n == 0:
+        return 0
+    table = [[0 for x in range(n)] for y in range(n)]
+    max_length = 1
+    for i in range(n):
+        table[i][i] = True
+    start = 0
+    for i in range(n-1):
+        if (str[i] == str[i+1]):
+            table[i][i+1] = True
+            start = i
+            max_length = 2
+        for k in range(3, n+1):
+            for i in range(n - k + 1):
+                j = i + k - 1
+                if (table[i+1][j-1] and str[i] == str[j]):
+                    table[i][j] = True
+                    if k > max_length:
+                        start = i
+                        max_length = k
+    return max_length

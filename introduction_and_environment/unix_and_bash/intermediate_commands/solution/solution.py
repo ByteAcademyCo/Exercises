04f8d2a116570637fbdb74811d18e2449919226b
dknight <knight1seven@gmail.com>
2021-01-27 16:14:57 -0500

    day one exercises

--- introduction_and_environment/unix_and_bash/intermediate_commands/solution/solution.py
+++ introduction_and_environment/unix_and_bash/intermediate_commands/solution/solution.py
@@ -1,7 +1,7 @@
 
 # Assign the correct strings below
-ONE = 
-TWO = 
-THREE = 
-FOUR = 
-FIVE = 
+ONE = 'mkdir testdir && cd testdir'
+TWO = 'cat textfile.txt | grep and'
+THREE = 'ls >> contents.txt'
+FOUR = 'cd current || mkdir current'
+FIVE = 'pwd > current_path.txt'

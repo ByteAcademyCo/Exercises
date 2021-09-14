08b74ccaaa2b703585ead47f3c5bcaf4fd212f29
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-09-14 08:05:20 -0400

    Update solution.py

--- introduction_to_python/functions/1_below_100/solution/solution.py
+++ introduction_to_python/functions/1_below_100/solution/solution.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+num = int()
+def below_100(num):
+    if num < 100:
+        return("less than 100")
+    else:
+        return("greater than 100")
+    
+below_100(50)
+below_100(200)

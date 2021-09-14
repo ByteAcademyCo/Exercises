326af490e807acd5b24c250f01d3c5f767aa26af
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-09-14 08:17:05 -0400

    Update solution.py

--- introduction_to_python/functions/3_divisible_by_3/solution/solution.py
+++ introduction_to_python/functions/3_divisible_by_3/solution/solution.py
@@ -1,1 +1,9 @@
 # Write your code here
+
+def divisible_by_3(*args):
+    div_3 = []
+    for num in args:
+        if num % 3 == 0:
+            return  div_3.append(num) 
+
+print(divisible_by_3(2,3,5,6,9))

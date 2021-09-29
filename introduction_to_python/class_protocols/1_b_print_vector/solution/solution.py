e0a0a13fce2eb373d9f61febce9ad46b6dd0607f
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-09-29 14:09:58 -0400

    Update solution.py

---
+++ introduction_to_python/class_protocols/1_b_print_vector/solution/solution.py
@@ -0,0 +1,13 @@
+class Vector3D:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'x = {self.x}, y = {self.y}, z = {self.z}'
+
+
+vector1 = Vector3D(3, 4, 2)
+print(vector1)

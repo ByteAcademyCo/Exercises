7fcac4b485d7545f931fc4da9132758a4f97f224
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-09-29 14:11:34 -0400

    Update solution.py

---
+++ introduction_to_python/class_protocols/2_a_add_vectors/solution/solution.py
@@ -0,0 +1,12 @@
+class Vector3D:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        z = self.z + other.z
+        return Vector3D(x, y, z)
